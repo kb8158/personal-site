979b710e3ee3f1726e279f1759d365332cc9b4da
Kate Brown <kbrown8158@gmail.com>
2017-04-03 18:31:31 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Kate Brown</h2>
+        </div>
+        <p className="App-intro">
+          <h1> Hi, I am Kate! </h1>
+          I am a recent graduate of Launch Academy, a Boston based programming bootcamp with a background in Applied Economics.
+          I love to solve problems and increase efficiency through code ands streamlining processes. When not coding you can find
+          me hiking, at the dog park or new local brewery.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
