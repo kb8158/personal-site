feffe8b53a9ec81bd1fa225cbf53458162d3e3e5
Kate Brown <kbrown8158@gmail.com>
2017-04-03 20:28:49 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Kate Brown</h2>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <h1> Hi, I'm Kate! </h1>
+              I am a recent graduate of Launch Academy, a Boston based programming bootcamp with a background in Applied Economics.
+              I love to solve problems and increase efficiency through code ands streamlining processes. When I'm not coding you can find
+              me hiking, at the dog park or new local brewery.
+            </div>
+          </div>
         </div>
-        <p className="App-intro">
-          <h1> Hi, I am Kate! </h1>
-          I am a recent graduate of Launch Academy, a Boston based programming bootcamp with a background in Applied Economics.
-          I love to solve problems and increase efficiency through code ands streamlining processes. When not coding you can find
-          me hiking, at the dog park or new local brewery.
-        </p>
       </div>
     );
   }
